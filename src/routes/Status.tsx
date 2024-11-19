d81332f8ac0d413da5f4f900a69139351b8a2ce3
TaegeonKim <aksen5240@gmail.com>
2024-11-20 00:37:56 +0900

    🔧 Set absolute path and add lint rules

--- src/routes/Status.tsx
+++ src/routes/Status.tsx
@@ -1,14 +1,23 @@
 import * as C from '../styles/CommonStyle';
 import * as S from '../styles/StatusStyle';
+import {dummyApplicationList} from '@/const/dummy_data/dummy_application_list';
 
 function Status() {
+  const applicationList = dummyApplicationList;
+  console.log(
+    '🚀 ~ file: Status.tsx:6 ~ Status ~ applicationList:',
+    applicationList,
+  );
+
   return (
     <>
       <C.Page>
         <C.Center>
           <S.Background>
             <C.PageSpace>
-              <S.Status></S.Status>
+              <S.Status>
+                <div>hello</div>
+              </S.Status>
             </C.PageSpace>
           </S.Background>
         </C.Center>
