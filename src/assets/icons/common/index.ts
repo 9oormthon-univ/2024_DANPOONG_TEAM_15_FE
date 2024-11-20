65ac724c649ff01dd5cca8a64a6613a02a605765
TaegeonKim <aksen5240@gmail.com>
2024-11-20 18:41:03 +0900

    :sparkles: Implement mypage UI

--- src/assets/icons/common/index.ts
+++ src/assets/icons/common/index.ts
@@ -1,6 +1,15 @@
 import BackXIcon from './back-x.svg?react';
 import ArrowRightIcon from './arrow-right.svg?react';
 import IvoryIcon from './ivory.svg?react';
+import IvoryTextIcon from './ivory-text.svg?react';
 import ArrowLeftIcon from './arrow-left.svg?react';
+import IvoryRemoveBgIcon from './ivory-remove-bg.svg?react';
 
-export {BackXIcon, ArrowRightIcon, IvoryIcon, ArrowLeftIcon};
+export {
+  BackXIcon,
+  ArrowRightIcon,
+  IvoryIcon,
+  IvoryTextIcon,
+  ArrowLeftIcon,
+  IvoryRemoveBgIcon,
+};
