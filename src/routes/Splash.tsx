9406fbe3455bca1d0eba9aaeff4c1d0157ad4a0a
Minseo Kim <marimang0602@g.skku.edu>
2024-11-23 16:15:02 +0900

    ✨ Implement create UI for login (#27)
    * feat: Login Page
    
    * :recycle: Splash, Login 경로 수정
    
    * fix: signin page error 해결

--- src/routes/Splash.tsx
+++ src/routes/Splash.tsx
@@ -1,9 +1,20 @@
+import {useNavigate} from 'react-router-dom'; // useNavigate 추가
 import * as C from '../styles/CommonStyle';
 import * as S from '../styles/SplashStyle';
 import {IvoryIcon} from '@/assets/icons/common';
 import {IvoryTextIcon} from '@/assets/icons/common';
 
 function Main() {
+  const navigate = useNavigate(); // 네비게이터 초기화
+
+  const handleLoginClick = () => {
+    navigate('/login'); // 로그인 페이지로 이동
+  };
+
+  const handleStartClick = () => {
+    navigate('/create-account'); // /create-user 페이지로 이동
+  };
+
   return (
     <C.Page>
       <C.Center>
@@ -15,10 +26,11 @@ function Main() {
               <IvoryTextIcon />
             </S.IconWrapper>
             <S.Footer>
-              <S.Main>바로 시작하기</S.Main>
+              {/* 바로 시작하기 버튼 */}
+              <S.Main onClick={handleStartClick}>바로 시작하기</S.Main>
               <S.LoginContainer>
                 <S.Logintext>계정이 있으신가요?</S.Logintext>
-                <S.LoginLink href="/login">로그인</S.LoginLink>
+                <S.LoginLink onClick={handleLoginClick}>로그인</S.LoginLink>
               </S.LoginContainer>
             </S.Footer>
           </C.PageSpace>
--- src/styles/LoginStyle.ts
+++ src/styles/LoginStyle.ts
@@ -1,8 +1,116 @@
 import styled from 'styled-components';
-// import palette from "../lib/colorPalette";
 
 export const Background = styled.div`
-  background-color: #FFFFFF;
+  background-color: #ffffff;
 `;
 
-export const Login = styled.div``;
+export const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin-top: 100px;
+  margin-bottom: 50px;
+  text-align: center;
+  flex-grow: 2;
+  gap: 8px;
+`;
+
+export const IvoryText = styled.div`
+  font-size: 12px;
+  font-family: 'Pretendard', sans-serif;
+  font-weight: 300;
+  color: #b45a5a;
+  line-height: 1.5;
+  text-align: center;
+  margin-bottom: -20px;
+`;
+
+export const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  text-align: center;
+  gap: 10px;
+`;
+
+export const Text = styled.div`
+  font-size: 16px;
+  font-family: 'Pretendard', sans-serif;
+  font-weight: 300;
+  color: #333333;
+  line-height: 1;
+  text-align: left;
+  width: 80%;
+  margin-left: -20px;
+`;
+
+export const Input = styled.input`
+  width: 80%;
+  height: 40px;
+  background: #f3f3f3;
+  border-radius: 12px;
+  padding: 14px 16px;
+  color: #333333;
+  font-size: 16px;
+  font-family: 'Pretendard', sans-serif;
+  font-weight: 400;
+  line-height: 24px;
+  border: none;
+  overflow: hidden;
+
+  &::placeholder {
+    color: #999999;
+  }
+
+  &:focus {
+    outline: none;
+    border: 1px solid #e07a5f;
+  }
+`;
+interface ButtonProps {
+  isActive: boolean;
+}
+
+export const LoginButton = styled.button<ButtonProps>`
+  width: 80%;
+  height: 56px;
+  padding-top: 16px;
+  padding-bottom: 16px;
+  background: ${({isActive}) => (isActive ? '#fc986c' : '#dfe2e5')};
+  color: ${({isActive}) => (isActive ? 'white' : '#999999')};
+  border-radius: 8px;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  cursor: ${({isActive}) => (isActive ? 'pointer' : 'not-allowed')};
+  transition:
+    background-color 0.3s ease,
+    color 0.3s ease;
+  position: absolute;
+  bottom: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+
+  &:hover {
+    background: ${({isActive}) => (isActive ? '#e07a5f' : '#dfe2e5')};
+  }
+
+  &:active {
+    background: ${({isActive}) => (isActive ? '#c76349' : '#dfe2e5')};
+  }
+
+  div {
+    text-align: center;
+    font-size: 16px;
+    font-family: 'Pretendard', sans-serif;
+    font-weight: 600;
+    line-height: 24px;
+    word-wrap: break-word;
+  }
+`;
