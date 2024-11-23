332ccce26e0d66f523d3fe9193dab472acbb6e9c
daram62 <97465078+daram62@users.noreply.github.com>
2024-11-23 15:21:46 +0900

    feat: Splash page

--- src/routes/Splash.tsx
+++ src/routes/Splash.tsx
@@ -1,20 +1,31 @@
 import * as C from '../styles/CommonStyle';
 import * as S from '../styles/SplashStyle';
+import {IvoryIcon} from '@/assets/icons/common';
+import {IvoryTextIcon} from '@/assets/icons/common';
 
-function Splash() {
+function Main() {
   return (
-    <>
-      <C.Page>
-        <C.Center>
-          <S.Background>
-            <C.PageSpace>
-              <S.Splash></S.Splash>
-            </C.PageSpace>
-          </S.Background>
-        </C.Center>
-      </C.Page>
-    </>
+    <C.Page>
+      <C.Center>
+        <S.Background>
+          <C.PageSpace>
+            <S.IconWrapper>
+              <IvoryIcon />
+              <S.Text>우리 아이의 안심케어를 약속하는</S.Text>
+              <IvoryTextIcon />
+            </S.IconWrapper>
+            <S.Footer>
+              <S.Main>바로 시작하기</S.Main>
+              <S.LoginContainer>
+                <S.Logintext>계정이 있으신가요?</S.Logintext>
+                <S.LoginLink href="/login">로그인</S.LoginLink>
+              </S.LoginContainer>
+            </S.Footer>
+          </C.PageSpace>
+        </S.Background>
+      </C.Center>
+    </C.Page>
   );
 }
 
-export default Splash;
+export default Main;
--- src/styles/SplashStyle.ts
+++ src/styles/SplashStyle.ts
@@ -1,8 +1,83 @@
 import styled from 'styled-components';
-// import palette from "../lib/colorPalette";
 
 export const Background = styled.div`
-  background-color: #FFEADC;
+  background: #ffeadc;
 `;
 
-export const Splash = styled.div``;
+export const IconWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  text-align: center;
+  margin-top: 200px;
+  gap: 16px;
+`;
+
+export const Text = styled.div`
+  font-size: 14px;
+  font-family: 'Pretendard', sans-serif;
+  font-weight: 500;
+  color: #b45a5a;
+  line-height: 1.5;
+  text-align: center;
+  margin-bottom: -20px;
+`;
+
+export const Footer = styled.div`
+  position: absolute;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
+  padding: 60px 16px;
+  background-color: #ffeadc;
+`;
+
+export const Main = styled.button`
+  width: 335px;
+  height: 56px;
+  background: #fc986c;
+  border-radius: 12px;
+  color: white;
+  font-size: 16px;
+  font-family: 'Pretendard', sans-serif;
+  font-weight: 600;
+  line-height: 24px;
+  text-align: center;
+  cursor: pointer;
+  border: none;
+
+  &:hover {
+    background: #e07a5f;
+  }
+`;
+
+export const LoginContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 4px;
+`;
+
+export const Logintext = styled.span`
+  font-size: 14px;
+  font-family: 'Pretendard', sans-serif;
+  font-weight: 400;
+  color: #333333;
+`;
+
+export const LoginLink = styled.a`
+  font-size: 14px;
+  font-family: 'Pretendard', sans-serif;
+  font-weight: 600;
+  color: #fc986c;
+  cursor: pointer;
+  text-decoration: underline;
+
+  &:hover {
+    color: #e07a5f;
+  }
+`;
