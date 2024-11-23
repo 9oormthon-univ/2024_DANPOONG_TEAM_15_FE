baf575dcac4417af1e75f19f5e16d8ebb8d42819
TaegeonKim <aksen5240@gmail.com>
2024-11-24 03:13:46 +0900

    :sparkles: Integrate status detail page api

--- src/utils/mainApi.tsx
+++ src/utils/mainApi.tsx
@@ -36,3 +36,36 @@ export const getChildStatus = async (childId: string) => {
     }
   }
 };
+
+export const getChildDetailStatus = async (applyId: string) => {
+  try {
+    const token = localStorage.getItem('accessToken');
+
+    if (!token) {
+      throw new Error('로그인이 필요합니다.');
+    }
+
+    const response = await axios.get(`${BASE_URL}/apply/${applyId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (response.status === 200 && response.data.success) {
+      console.log('신청 조회 성공:', response.data.message);
+      return response.data;
+    }
+  } catch (error: any) {
+    if (axios.isAxiosError(error) && error.response) {
+      console.error(
+        '신청 조회 실패:',
+        `상태코드: ${error.response.status}, 메시지:`,
+        error.response.data.message,
+      );
+      throw new Error(error.response.data.message);
+    } else {
+      console.error('신청 조회 중 알 수 없는 오류 발생:', error);
+      throw new Error('신청 조회 중 알 수 없는 오류가 발생했습니다.');
+    }
+  }
+};
