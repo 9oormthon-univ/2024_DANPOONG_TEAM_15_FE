3ec71030da85f76a3043cc109be556c51054a4a8
pookey1104 <pookey1104@naver.com>
2024-12-06 06:52:17 +0900

    ✨ Implement custom 404 page UI (#54)

--- src/routes/Non.tsx
+++ src/routes/Non.tsx
@@ -1,14 +1,51 @@
-import * as C from '../styles/CommonStyle';
-import * as S from '../styles/NonStyle';
+import {useNavigate} from 'react-router-dom';
+import * as C from '@/styles/CommonStyle';
+import * as S from '@/styles/NonStyle';
+import CustomIcon from '@/assets/icons/common/custom-404.svg';
 
 function Non() {
+  const navigate = useNavigate();
+
+  const handleNavLinkClick = (): void => {
+    const accessToken = localStorage.getItem('accessToken');
+    const authority = localStorage.getItem('authority');
+
+    if (!accessToken) {
+      navigate('/');
+    }
+
+    // 권한에 따라 경로 변경
+    if (authority === 'ROLE_CAREGIVER') {
+      navigate('/caregiver-main');
+    } else {
+      navigate('/main');
+    }
+  };
+
   return (
     <>
       <C.Page>
         <C.Center>
           <S.Background>
             <C.PageSpace>
-              <S.Non></S.Non>
+              <S.PageSpace>
+                <S.Container>
+                  <S.CustomIcon src={CustomIcon} alt="404 페이지" />
+                  <S.TextContainer>
+                    <S.Title>찾을 수 없는 페이지 입니다.</S.Title>
+                    <S.Text>
+                      요청하신 페이지가 사라졌거나,
+                      <br />
+                      잘못된 경로를 이용하셨어요 :)
+                    </S.Text>
+                  </S.TextContainer>
+                </S.Container>
+                <S.FooterContainer>
+                  <S.Button onClick={() => handleNavLinkClick()}>
+                    홈으로 가기
+                  </S.Button>
+                </S.FooterContainer>
+              </S.PageSpace>
             </C.PageSpace>
           </S.Background>
         </C.Center>
--- src/styles/NonStyle.ts
+++ src/styles/NonStyle.ts
@@ -1,8 +1,75 @@
 import styled from 'styled-components';
-// import palette from "../lib/colorPalette";
+import {COLOR} from '@/const/color';
 
 export const Background = styled.div`
-  background-color: #FFFFFF;
+  background-color: #ffffff;
 `;
 
-export const Non = styled.div``;
+export const PageSpace = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100dvh;
+`;
+
+export const Container = styled.div`
+  padding: 156px 20px 0;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  gap: 28px;
+`;
+
+export const CustomIcon = styled.img`
+  width: 174px;
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 4px;
+`;
+
+export const Title = styled.div`
+  color: ${COLOR.BLACK_03};
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 20px;
+  letter-spacing: -0.35px;
+`;
+
+export const Text = styled.div`
+  color: ${COLOR.GRAY_04};
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 18px;
+  letter-spacing: -0.3px;
+`;
+
+export const FooterContainer = styled.div`
+  padding: 16px 20px 50px;
+  background-color: ${COLOR.WHITE_01};
+`;
+
+export const Button = styled.div`
+  border-radius: 8px;
+  background-color: ${COLOR.ORANGE_01};
+  width: 100%;
+  padding: 16px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+
+  color: ${COLOR.WHITE_01};
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 24px;
+  letter-spacing: -0.4px;
+
+  &:hover {
+    background-color: ${COLOR.ORANGE_02};
+    transition: background-color 0.2s;
+  }
+`;
